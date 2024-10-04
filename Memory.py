b6030449771e5d55048d42b951364a537b8bb486
Alonso111204 <A01782379@tec.mx>
2024-10-04 08:45:11 -0600

    Update Memory.py

--- Memory.py
+++ Memory.py
@@ -1,5 +1,6 @@
 import random
 
+
 card_values= []
 
 for i in range(1,19):
@@ -9,15 +10,17 @@ for i in range(1,19):
 random.shuffle(card_values)
 print(card_values)
 
+card_values=[card_values[:6],card_values[6:12],card_values[12:18],card_values[18:24],card_values[24:30],card_values[30:36]]
+
+ast= []
 
 
-def draw(n):
-    for i in range(n):
-        print(i)
-        for j in range(n-1):
-            print("* ", end="")
+for i in range(1,37):
+    ast += ["*"]
 
-            
-draw(7)
-p1= 0
-p2 = 0
+ast = [ast[:6],ast[6:12],ast[12:18],ast[18:24],ast[24:30],ast[30:36]]
+for i in ast:
+    print()
+    for j in i:
+        print(j,end=" ")
+       
